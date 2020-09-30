70a80c24de20de9d338c31229abecf6af33788cf
ThePassionateCoders <69468377+ThePassionateCoders@users.noreply.github.com>
2020-10-01 00:07:07 +0530

    Error Handling
    I have applied a check so that user shoud not be able to give an empty value

--- palindrome.py
+++ palindrome.py
@@ -7,3 +7,18 @@ if c == a :
 	
 else:
 	print('It is not a palindrome')
+print("What you want to check for palindrome?")
+a = input()
+while True:
+	if a.replace(" ", "") != "":
+		c = a[ : : -1]
+			
+		if c == a :
+			print('It is a palindrome')
+			break
+			
+		else:
+			print('It is not a palindrome')
+			break
+	else:
+		a=input("Please give some value:\n")
